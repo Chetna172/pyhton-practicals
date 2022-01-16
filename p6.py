dc0dcffbe0c5191faf98fa27bce7a41e7d348367
Chetna172 <97662378+Chetna172@users.noreply.github.com>
2022-01-16 03:37:32 -0500

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,6 @@
+SET = {'H', 'e', 'l', 'l', 'o'}
+SET.add('!')
+print("Letters are:", SET)
+print("Set before clear:", SET)
+print("Set after clear:", SET.clear()
+print("D21CE172, CHETNA MAKWANA");
---
+++ p7.py
@@ -0,0 +1,6 @@
+
+set = {'H', 'e', 'l', 'l', 'o'}
+set.remove('o')
+print("Letters are:", set)
+
+print("D21CE172, CHETNA MAKWANA");
